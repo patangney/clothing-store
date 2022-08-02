bc8b992f69bbd93eefc05b14c51e1fc502263734
Patrick Tangney <ptangney@outlook.com>
2022-08-02 11:49:19 +0100

    setup page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,50 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="categories-container">
+      <div className="category-container">
+        {/* img src */}
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* img src */}
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* img src */}
+        <div className="category-body-container">
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* img src */}
+        <div className="category-body-container">
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* img src */}
+        <div className="category-body-container">
+          <h2>Womens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* img src */}
+        <div className="category-body-container">
+          <h2>Mens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
