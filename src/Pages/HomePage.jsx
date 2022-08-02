d0fdb27308c6d3c0b2a2406a54aa59fc366f7d2b
Patrick Tangney <ptangney@outlook.com>
2022-08-02 12:10:11 +0100

    setup scss

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -15,33 +15,37 @@ const HomePage = () => {
     {
       id: 2,
       title: 'Jackets',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-      linkUrl: 'hats'
+      imageUrl: 'https://i.ibb.co/cvpntL1/jackets.png',
+      linkUrl: 'jackets'
     },
     {
       id: 3,
       title: 'Sneakers',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-      linkUrl: 'hats'
+      imageUrl: 'https://i.ibb.co/cvpntL1/sneakers.png',
+      linkUrl: 'sneakers'
     },
     {
       id: 4,
       title: 'Womens',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-      linkUrl: 'hats'
+      imageUrl: 'https://i.ibb.co/cvpntL1/womens.png',
+      linkUrl: 'womens'
     },
     {
       id: 5,
       title: 'Mens',
       imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-      linkUrl: 'hats'
+      linkUrl: 'mens'
     },
   ]
   return (
     <div className="categories-container">
+      {/* ---- destructure title, id ---- */}
       {categories.map(({title, id}) => (
         <div key={id} className="category-container">
           {/* ---- image ---- */}
+          <div className="background-image">
+
+          </div>
           <div className="category-body-container">
             <h2>{title}</h2>
             <p>Shop Now</p>
